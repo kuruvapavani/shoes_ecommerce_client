f7e48e1d0e6549ec1d39db0f482d74b6c8e9120f
kuruvapavani <pavanikuruva2109@gmail.com>
2024-04-04 15:24:54 +0530

    setup navbar

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -1,9 +1,193 @@
-import React from 'react'
+import React, { useState ,useEffect } from 'react';
+import { Fragment } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping, faSun } from '@fortawesome/free-solid-svg-icons';
+import img from "./logo.png";
+import { faMoon } from '@fortawesome/free-regular-svg-icons';
+const navigation = [
+  { name: 'New Products', href: '#new', current: true },
+  { name: 'Offers', href: '#offers', current: false },
+  { name: 'Customer Service', href: '/services', current: false }
+];
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
 }
 
-export default Header
+const Header = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    // Update body class when darkMode changes
+    if (darkMode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, [darkMode]);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    // You can add logic to toggle dark mode here
+  };
+
+  return (
+    <div className={`bg-${darkMode ? 'gray-900' : 'white'}`}>
+      <Disclosure as="nav" className={`bg-${darkMode ? 'gray-900' : 'white'}`}>
+        {({ open }) => (
+          <>
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+              <div className="relative flex h-16 items-center justify-between">
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                  <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span className="absolute -inset-0.5" />
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
+                <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                  <div className="flex flex-shrink-0 items-center">
+                    <a href='/'>
+                      <img
+                        className="h-10 w-auto"
+                        src={img}
+                        alt="Your Company"
+                      />
+                    </a>
+                  </div>
+                  <div className="hidden sm:ml-6 sm:block">
+                    <div className="flex space-x-4">
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className={classNames(
+                            item.current ? 'bg-gray-400 text-white' : 'text-gray-500 hover:bg-gray-300 hover:text-white',
+                            'rounded-md px-3 py-2 text-sm font-medium'
+                          )}
+                          aria-current={item.current ? 'page' : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                  <a href='/cart'>
+                    <button
+                      type="button"
+                      className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 h-8 w-8"
+                    >
+                      <span className="absolute -inset-1.5" />
+                      <span className="sr-only">View cart</span>
+                      <FontAwesomeIcon icon={faCartShopping} className="h-4" aria-hidden="true" />
+                    </button>
+                  </a>
+                  <Menu as="div" className="relative ml-3">
+                    <div>
+                      <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <span className="absolute -inset-1.5" />
+                        <span className="sr-only">Open user menu</span>
+                        <img
+                          className="h-8 w-8 rounded-full"
+                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                          alt=""
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition-transform ease-out duration-200"
+                      enterFrom="opacity-0 translate-y-[-10px]"
+                      enterTo="opacity-100 translate-y-0"
+                      leave="transition-transform ease-in duration-150"
+                      leaveFrom="opacity-100 translate-y-0"
+                      leaveTo="opacity-0 translate-y-[-10px]"
+                    >
+
+
+                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md  py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="/profile"
+                              className={`${classNames(active ? '' : 'block px-4 py-2 text-sm text-gray-700')} ${darkMode?'bg-black text-white':'bg-white text-black'}`}
+                            >
+                             <span>Your Profile</span>
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="/memberships"
+                              className={`${classNames(active ? '' : 'block px-4 py-2 text-sm text-gray-700')} ${darkMode?'bg-black text-white':'bg-white text-black'}`}
+                            >
+                            <span>Memberships</span>
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="/logout"
+                              className={`${classNames(active ? '' : 'block px-4 py-2 text-sm text-gray-700')} ${darkMode?'bg-black text-white':'bg-white text-black'}`}
+                            >
+                            <span>Sign out</span>
+                            </a>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                </div>
+              </div>
+            </div>
+
+            <Disclosure.Panel className="sm:hidden">
+              <div className="space-y-1 px-2 pb-3 pt-2">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'block rounded-md px-3 py-2 text-base font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+
+      {/* Dark mode toggle button */}
+      <div className="fixed bottom-4 right-4">
+        <button
+          className={`rounded-full bg-gray-800 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 h-12 w-12 flex items-center justify-center`}
+          onClick={toggleDarkMode}
+        >
+          <span className="sr-only">Toggle Dark Mode</span>
+          {darkMode ? (
+            <FontAwesomeIcon icon={faSun} />
+          ) : (
+            <FontAwesomeIcon icon={faMoon} />
+          )}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
